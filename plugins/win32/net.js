1c41fe486ff8904bb4e8a192073910a9c775e465
Javier Ailbirt <jailbirt@gmail.com>
2016-08-05 00:38:11 -0300

    Silence net and disk i/o stats on win

--- plugins/win32/net.js
+++ plugins/win32/net.js
@@ -39,46 +39,10 @@ net.prototype.get = function(nstat, callback) {
   var data = this.data;
   var skip = 2;
   var total = initdevrow();
-  nstat.lines(
-    '/proc/net/dev',
-    function(line) {
-      if (--skip >= 0) {
-        return;
-      }
-      line = nstat.trim(line).replace(':',' ');
-      if (line.length === 0) {
-        return;
-      }
-      var columns = nstat.split(line);
-      var dev = columns.shift();
-      var recv = Number(columns[0]);
-      var send = Number(columns[Math.floor(columns.length/2)]);
-      
-      var devcurr = getdev(curr, dev);
-      var devprev = getdev(prev, dev);
-      var devdata = getdev(data, dev);
-      
-      devcurr.receive = recv;
-      devcurr.send = send;
-      
-      
-      devdata.receive = devprev.receive === 0 ? 0 : recv - devprev.receive;
-      devdata.send = devprev.send === 0 ? 0 : send - devprev.send;
-      
-      total.receive += devdata.receive;
-      total.send += devdata.send;
-      
-    }, function(err) {
       self.data.total = total;
       self.prev = self.curr;
       self.curr = initrow();
-      if (err) {
-        callback(err);
-      } else {
-        callback(null, self.data);
-      }
-    }
-  );
+      callback(null, self.data);
 };
 
 module.exports = new net;
